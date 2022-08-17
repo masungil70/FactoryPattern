ac919950c3b0c5e7d6aafaf1fbf8b3bfaf4bacb6
마성일 <masungil70@gmail.com>
2022-08-17 16:44:33 +0900

    Cat 만 구현한 것임 private 구조체를 선언하여 클래스 factory mehod  구현하는 예제 코드

--- FactoryPattern/FactoryPattern6.cpp
+++ FactoryPattern/FactoryPattern6.cpp
@@ -14,10 +14,13 @@ enum class AnimalType {
 
 class Animal {
 public:
+    virtual ~Animal() {
+        cout << "Animal() 소멸자 호출\n";
+    }
     virtual void speak() = 0;
 
     static Animal* of(AnimalType type);
-//    static Animal* factoryAnimal(AnimalType type);
+    //    static Animal* factoryAnimal(AnimalType type);
 };
 
 void hey(Animal& animal) {
@@ -25,9 +28,13 @@ void hey(Animal& animal) {
 }
 
 class Cat : public Animal {
+private:
     Cat() {
     }
 public:
+    ~Cat() {
+        cout << "Cat() 소멸자 호출\n";
+    }
     static Animal* createAnimal() {
         return new Cat;
     }
@@ -37,14 +44,31 @@ public:
 };
 
 class Dog : public Animal {
+private:
+    Dog() {}
 public:
+    ~Dog() {
+        cout << "Dog() 소멸자 호출\n";
+    }
+
+    static Animal* createAnimal() {
+        return new Dog;
+    }
     virtual void speak() override {
         cout << "멍멍멍~~~" << endl;
     }
 };
 
 class Sheep : public Animal {
+private:
+    Sheep() {}
 public:
+    ~Sheep() {
+        cout << "Sheep() 소멸자 호출\n";
+    }
+    static Animal* createAnimal() {
+        return new Sheep;
+    }
     virtual void speak() override {
         cout << "매~~ 매~~" << endl;
     }
@@ -52,7 +76,15 @@ public:
 
 //추가된 부분 --> Open Close 문제 없음 
 class Cow : public Animal {
+private:
+    Cow() {}
 public:
+    ~Cow() {
+        cout << "Cow() 소멸자 호출\n";
+    }
+    static Animal* createAnimal() {
+        return new Cow;
+    }
     virtual void speak() override {
         cout << "음매 음매~~" << endl;
     }
@@ -60,33 +92,32 @@ public:
 
 
 Animal* FactoryAnimal(AnimalType type) {
-    
-    switch(type) {
+
+    switch (type) {
     case AnimalType::CatType:
         return Cat::createAnimal();
     case AnimalType::DogType:
-        return new Dog();
+        return Dog::createAnimal();
     case AnimalType::SheepType:
-        return new Sheep();
+        return Sheep::createAnimal();
     case AnimalType::CowType:
-        //추가된 부분 --> Open Close 문제 있음 -> 단 객체지향 프로그램에서 이것은 예외로 한다
-        return new Cow();
+        return Cow::createAnimal();
     }
     throw exception("객체를 생성할 수 없습니다");
 }
 
 Animal* Animal::of(AnimalType type) {
-//Animal* Animal::factoryAnimal(AnimalType type) {
-        switch (type) {
+    //Animal* Animal::factoryAnimal(AnimalType type) {
+    switch (type) {
     case AnimalType::CatType:
         return Cat::createAnimal();
     case AnimalType::DogType:
-        return new Dog();
+        return Dog::createAnimal();
     case AnimalType::SheepType:
-        return new Sheep();
+        return Sheep::createAnimal();
     case AnimalType::CowType:
         //추가된 부분 --> Open Close 문제 있음 -> 단 객체지향 프로그램에서 이것은 예외로 한다
-        return new Cow();
+        return Cow::createAnimal();
     }
     throw exception("객체를 생성할 수 없습니다");
 }
@@ -98,25 +129,25 @@ public:
         case AnimalType::CatType:
             return Cat::createAnimal();
         case AnimalType::DogType:
-            return new Dog();
+            return Dog::createAnimal();
         case AnimalType::SheepType:
-            return new Sheep();
+            return Sheep::createAnimal();
         case AnimalType::CowType:
             //추가된 부분 --> Open Close 문제 있음 -> 단 객체지향 프로그램에서 이것은 예외로 한다
-            return new Cow();
+            return Cow::createAnimal();
         }
         throw exception("객체를 생성할 수 없습니다");
     }
 };
 
 void someA() {
-//    Animal* pAnimal = Animal::factoryAnimal(AnimalType::CatType);
-    //Animal* pAnimal = Animal::of(AnimalType::CatType);
+    //    Animal* pAnimal = Animal::factoryAnimal(AnimalType::CatType);
+        //Animal* pAnimal = Animal::of(AnimalType::CatType);
 
-    //if (pAnimal) {
-    //    hey(*pAnimal);
-    //    delete pAnimal;
-    //}
+        //if (pAnimal) {
+        //    hey(*pAnimal);
+        //    delete pAnimal;
+        //}
     shared_ptr<Animal> pAnimal(Animal::of(AnimalType::CatType));
     if (pAnimal) {
         hey(*pAnimal);
@@ -124,7 +155,7 @@ void someA() {
 }
 
 void someB() {
-//    Animal* pAnimal = FactoryAnimal(AnimalType::CowType);
+    //    Animal* pAnimal = FactoryAnimal(AnimalType::CowType);
     Animal* pAnimal = Animal::of(AnimalType::CowType);
 
     if (pAnimal) {
@@ -133,14 +164,14 @@ void someB() {
     }
 }
 
-void main() {
+int main() {
     Animal* p = Animal::of(AnimalType::CowType);
-    p = new Cow();
-
     p->speak();
     delete p;
 
-    someA();
-    someB();
+    //    someA();
+    //    someB();
+
+    return 0;
 }
 
--- FactoryPattern/FactoryPattern7.cpp
+++ FactoryPattern/FactoryPattern7.cpp
@@ -14,9 +14,12 @@ enum class AnimalType {
 
 class Animal {
 public:
+    virtual ~Animal() {
+        cout << "Animal() 소멸자 호출\n";
+    }
     virtual void speak() = 0;
 
-    static Animal* of(AnimalType type);
+    static shared_ptr<Animal> of(AnimalType type);
     //    static Animal* factoryAnimal(AnimalType type);
 };
 
@@ -25,11 +28,16 @@ void hey(Animal& animal) {
 }
 
 class Cat : public Animal {
-    Cat() {
-    }
+private:
+    struct _aCat {};
 public:
-    static Animal* createAnimal() {
-        return new Cat;
+    Cat(_aCat obj) {
+    }
+    ~Cat() {
+        cout << "Cat() 소멸자 호출\n";
+    }
+    static shared_ptr<Animal> createAnimal() {
+        return make_shared<Cat>(_aCat{});
     }
     virtual void speak() override {
         cout << "야옹~~~" << endl;
@@ -37,14 +45,31 @@ public:
 };
 
 class Dog : public Animal {
+private:
+    Dog() {}
 public:
+    ~Dog() {
+        cout << "Dog() 소멸자 호출\n";
+    }
+
+    static shared_ptr<Animal> createAnimal() {
+        return make_shared<Dog>();
+    }
     virtual void speak() override {
         cout << "멍멍멍~~~" << endl;
     }
 };
 
 class Sheep : public Animal {
+private:
+    Sheep() {}
 public:
+    ~Sheep() {
+        cout << "Sheep() 소멸자 호출\n";
+    }
+    static shared_ptr<Animal> createAnimal() {
+        return make_shared<Sheep>();
+    }
     virtual void speak() override {
         cout << "매~~ 매~~" << endl;
     }
@@ -52,58 +77,65 @@ public:
 
 //추가된 부분 --> Open Close 문제 없음 
 class Cow : public Animal {
+private:
+    Cow() {}
 public:
+    ~Cow() {
+        cout << "Cow() 소멸자 호출\n";
+    }
+    static shared_ptr<Animal> createAnimal() {
+        return make_shared<Cow>();
+    }
     virtual void speak() override {
         cout << "음매 음매~~" << endl;
     }
 };
 
 
-Animal* FactoryAnimal(AnimalType type) {
+shared_ptr<Animal> FactoryAnimal(AnimalType type) {
 
     switch (type) {
     case AnimalType::CatType:
         return Cat::createAnimal();
     case AnimalType::DogType:
-        return new Dog();
+        return Dog::createAnimal();
     case AnimalType::SheepType:
-        return new Sheep();
+        return Sheep::createAnimal();
     case AnimalType::CowType:
-        //추가된 부분 --> Open Close 문제 있음 -> 단 객체지향 프로그램에서 이것은 예외로 한다
-        return new Cow();
+        return Cow::createAnimal();
     }
     throw exception("객체를 생성할 수 없습니다");
 }
 
-Animal* Animal::of(AnimalType type) {
+shared_ptr<Animal> Animal::of(AnimalType type) {
     //Animal* Animal::factoryAnimal(AnimalType type) {
     switch (type) {
     case AnimalType::CatType:
         return Cat::createAnimal();
     case AnimalType::DogType:
-        return new Dog();
+        return Dog::createAnimal();
     case AnimalType::SheepType:
-        return new Sheep();
+        return Sheep::createAnimal();
     case AnimalType::CowType:
         //추가된 부분 --> Open Close 문제 있음 -> 단 객체지향 프로그램에서 이것은 예외로 한다
-        return new Cow();
+        return Cow::createAnimal();
     }
     throw exception("객체를 생성할 수 없습니다");
 }
 
 class FactoryAnimal {
 public:
-    static Animal* factoryAnimal(AnimalType type) {
+    static shared_ptr<Animal> factoryAnimal(AnimalType type) {
         switch (type) {
         case AnimalType::CatType:
             return Cat::createAnimal();
         case AnimalType::DogType:
-            return new Dog();
+            return Dog::createAnimal();
         case AnimalType::SheepType:
-            return new Sheep();
+            return Sheep::createAnimal();
         case AnimalType::CowType:
             //추가된 부분 --> Open Close 문제 있음 -> 단 객체지향 프로그램에서 이것은 예외로 한다
-            return new Cow();
+            return Cow::createAnimal();
         }
         throw exception("객체를 생성할 수 없습니다");
     }
@@ -125,22 +157,22 @@ void someA() {
 
 void someB() {
     //    Animal* pAnimal = FactoryAnimal(AnimalType::CowType);
-    Animal* pAnimal = Animal::of(AnimalType::CowType);
+    shared_ptr<Animal> pAnimal = Animal::of(AnimalType::CowType);
 
     if (pAnimal) {
         hey(*pAnimal);
-        delete pAnimal;
     }
 }
 
-void main() {
-    Animal* p = Animal::of(AnimalType::CowType);
-    p = new Cow();
+int main() {
+    shared_ptr<Animal> pCat = Cat::createAnimal();
 
+    shared_ptr<Animal> p = Animal::of(AnimalType::CowType);
     p->speak();
-    delete p;
 
-    someA();
-    someB();
+    //    someA();
+    //    someB();
+
+    return 0;
 }
 
